384f3cf10a19215a81ac6e31367f80ee79194b51
nsmeds <niilosmeds@gmail.com>
2017-05-01 16:31:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>RETASK</h1>
+                <h2>A To-Do Application built with React</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
