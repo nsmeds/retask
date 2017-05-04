1faa5b4c56587c880f63f0b5b910caaccf2f43bd
nsmeds <niilosmeds@gmail.com>
2017-05-04 11:18:08 -0700

    inputs and checkboxes update state

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,11 +4,19 @@ const Todos = props => {
 
     console.log('props from Todos', props);
 
-    let allTodos = props.todos.map((todo, index) => 
-        <li key={index}>
-            <input key={todo.id} name="title" value={todo.title} onChange={props.handleInputChange} />
-        </li>
-    )
+    let allTodos = props.todos.map((todo, index) => {
+        console.log(todo);
+        return (
+            <li key={index}>
+                <label>
+                    <span>{index + 1}</span>
+                    <input key={todo.id} id={todo.id} name="title" value={todo.title} onChange={props.handleInputChange} />
+                    <input type="checkbox" id={todo.id} name="completed" value={todo.completed} onChange={props.handleInputChange} />
+                    <span>{todo.completed ? 'Completed' : 'Incomplete'}</span>
+                </label>
+            </li>
+        )
+    })
 
     return (
         <ul>
